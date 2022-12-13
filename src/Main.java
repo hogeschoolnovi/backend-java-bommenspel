db7d18f99ed9f24f6cf860cbe93a6688e2653e12
MRensen <markrensen@hotmail.com>
2022-12-13 12:27:43 +0100

    comments

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,12 @@ public class Main {
         int bomb = random.nextInt(9);
         int input = -1;
 
+        // We gebruiken hier een while-loop. Een for-loop gebruiken we wanneer we door een lijst of array van waardes willen loopen.
+        // We hebben hier geen lijst met waardes om doorheen te loopen.
         while (input!=bomb) {
             System.out.println("\nKies een nummer van het keypad om in te drukken");
             input = scanner.nextInt();
+            // We gebruiken hier het best een if/else statement omdat we slechts 2 mogelijkheden hebben.
             if (input == bomb) {
                 Util.printExplosion();
             } else {
@@ -19,6 +22,7 @@ public class Main {
             }
         }
     }
+
 
 
 
